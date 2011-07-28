2e88cbf6b8ba650004285600fc728f4407799e4b
Jie Li <jacklee1985@gmail.com>
2011-07-28 05:26:45 -0400

    EmulabStarter updated.

--- EmulabStarter/run_starter.sh
+++ EmulabStarter/run_starter.sh
@@ -1,12 +1,9 @@
 HOST=`hostname -f`
 
-cd ~/$HOST/Test/EmulabStarter
 make
 make clean
 cp ./emustarter ~/$HOST/
 cp ./config ~/$HOST/
 cd ~/$HOST
-sleep 1
-sudo rm -rf ~/$HOST/Test
 sudo ./emustarter config
 
